721445f3ef85008655c92ec10b56ad485d90611e
Elisa Levy <elisalevygracia@gmail.com>
2023-02-10 16:54:22 +0100

    Added register form

---
+++ routes/registerRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const router = express.Router(); //we use the Router that comes with Express framework
+
+app.set("view engine", "pug");
+app.set("views", "views")
+
+router.get("/", (req, res, next) => {
+    
+    res.status(200).render("register");
+})
+
+module.exports = router;
