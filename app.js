375588117d97ed748b9b6dff426afa660e508e8c
Elisa Levy <elisalevygracia@gmail.com>
2023-02-10 16:28:12 +0100

    Added CSS login file

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3003;
-const middleware = require("./middleware")
+const middleware = require("./middleware");
+const path = require('path');
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
-app.set("views", "views")
+app.set("views", "views");
+
+// Line to specify anything inside pf this path, and whatever is inside the folder it's to be served as a static file
+app.use(express.static(path.join(__dirname, "public")))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
