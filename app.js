b137d2d46a9537e2fe90ee980c2095151fcba2c7
Elisa Levy <elisalevygracia@gmail.com>
2023-02-11 10:31:35 +0100

    Added login route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require("./middleware");
 const path = require('path');
 const bodyParser = require("body-parser");
 const mongoose = require("./database");
+const session = require("express-session");
 
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
@@ -16,6 +17,12 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use(session({
+    secret: "calico cats",
+    resave: true,
+    saveUninitialized: false
+}))
+
 // Routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
@@ -26,7 +33,8 @@ app.use("/register", registerRoute)
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
     const payload = {
-        pageTitle: "Home"
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
     }
 
     res.status(200).render("home", payload);
