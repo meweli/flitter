f25ca11e5aca525371d4c41afd0b118215a23a7a
Elisa Levy <elisalevygracia@gmail.com>
2023-02-12 13:33:12 +0100

    Added profile pic and submit buttom

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+
     firstName:{
         type: String,
         required: true,
@@ -28,8 +29,11 @@ const UserSchema = new Schema({
     password:{
         type: String,
         required: true
-    }
-    //profilePic: { type: String, default: "/images/profilePic.png"},
+    },
+    profilePic: { 
+        type: String, 
+        default: "/images/profilePic.jpg"
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
