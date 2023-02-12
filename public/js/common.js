bbcdcbd0c4435eeba8bed3957006c67dcbd52397
Elisa Levy <elisalevygracia@gmail.com>
2023-02-12 14:45:56 +0100

    Added API

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,28 @@
-$(document).ready(() => {
-    alert("MIAUUUUUU!")
+$("#postTextarea").keyup(event => {
+    const textbox = $(event.target);
+    const value = textbox.val().trim();
+    
+    const submitButtom = $("#submitPostButtom");
+
+    //if(submitButtom, length == 0) return alert("No submit buttom found");
+
+    if (value == "") {
+        submitButtom.prop("disabled", true);
+        return;
+    }
+
+    submitButtom.prop("disabled", false);
+})
+
+$("#submitPostButtom").click(() => {
+    const button = $("event.target");
+    const textbox = $("#postTextarea");
+
+    const data = {
+        //content: textbox.val()
+    }
+
+    $.post("/api/posts", data, (postData, status, xhr) => {
+        alert(postData);
+    })
 })
